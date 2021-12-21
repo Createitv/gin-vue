7a3b40457174480f0e9676813ea8b9f11f8c4adb
TalkBeCheap <xfy150150@gmail.com>
2021-12-21 11:12:46 +0800

    api分组

--- router/router.go
+++ router/router.go
@@ -3,15 +3,19 @@ package router
 import (
 	"fmt"
 	"github.com/createitv/gin-vue/controller/user"
-	"github.com/createitv/gin-vue/middleware"
+	"github.com/createitv/gin-vue/router/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func Run() {
 	r := gin.Default()
-	r.POST("/api/user/register", user.Register)
-	r.POST("/api/user/login", user.Login)
-	r.GET("api/user/info", middleware.AuthMiddleware(), user.Info)
+	userRoute := r.Group("/api/user")
+	{
+		userRoute.POST("/register", user.Register)
+		userRoute.POST("/login", user.Login)
+		userRoute.GET("/info", middleware.AuthMiddleware(), user.Info)
+
+	}
 	fmt.Println("server listening on 8080")
 	err := r.Run()
 	if err != nil {
